a7bec2cb054a32b2a322aab7fcde6e2943b8e41a
Daniel Fehrm <daniel@fehrm.se>
2023-01-06 16:18:05 +0100

    update to dotnet 7

--- API/DTOs/RegisterDTO.cs
+++ API/DTOs/RegisterDTO.cs
@@ -9,10 +9,10 @@ namespace API.DTOs
         public string Username { get; set; }
         [Required] public string KnownAs { get; set; }
         [Required] public string Gender { get; set; }
-        [Required] public DateTime DateofBirth { get; set; }
-        [Required] public string KnownAs { get; set; }
-        [Required] public string KnownAs { get; set; }
-        [Required] public string KnownAs { get; set; }
+        [Required] public DateOnly DateofBirth { get; set; }
+        // [Required] public string KnownAs { get; set; }
+        // [Required] public string KnownAs { get; set; }
+        // [Required] public string KnownAs { get; set; }
         [Required]
         [StringLength(8, MinimumLength = 4)]
         public string Password { get; set; }
